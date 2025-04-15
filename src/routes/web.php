1e770b169333b5a60c4eb7fae1f1e6ee14cbeb54
oghryatt <hirotaka.ogawa.03@outlook.jp>
2025-04-15 21:35:00 +0900

    プログラム変更

--- src/routes/web.php
+++ src/routes/web.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\LoginController;
 use App\Http\Requests\LoginRequest;
 use App\Http\Controllers\AdminController;
 use App\Http\Requests\AdminRequest;
+use App\Http\Controllers\IndexController;
 use App\Http\Controllers\ConfirmController;
 use App\Http\Requests\ConfirmRequest;
 
